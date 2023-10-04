d53be4a6730e20c25b3f8950c02409cb2d5a5cc1
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-10-04 18:32:49 +0300

    repositories are rdy

---
+++ src/main/java/hu/progmatic/hotel/repository/ReservationRepository.java
@@ -0,0 +1,11 @@
+package hu.progmatic.hotel.repository;
+
+import hu.progmatic.hotel.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+
+
+}
