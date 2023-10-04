653deb7c38e191079d553c34b1a76ea24865ba13
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-10-04 20:35:03 +0300

    db is rdy in room

--- src/main/java/hu/progmatic/hotel/model/Room.java
+++ src/main/java/hu/progmatic/hotel/model/Room.java
@@ -1,9 +1,6 @@
 package hu.progmatic.hotel.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import org.springframework.context.annotation.Bean;
 
 @Entity
@@ -11,6 +8,8 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "room_number")
+
     private String roomNumber;
     private int capacity;
     private double nightlyPrice;
